749077f7e5d9b8bd6c7edd58ab7cc05019fa0579
fep <franck.en.planck@gmail.com>
2019-11-11 11:52:46 +0100

    Erreur en pom..

---
+++ Appli/src/main/java/fr/hardwareInput/Keyboard.java
@@ -0,0 +1,22 @@
+package fr.hardwareInput;
+
+public class Keyboard {
+	
+
+	void key_callback(GLFWwindow window, int key, int scancode, int action, int mods)
+	{
+	    if (key == GLFW_KEY_E && action == GLFW_PRESS)
+	        activate_airship();
+	}
+ 
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
