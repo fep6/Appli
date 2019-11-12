5e00d29994b377dfcf95cc6541f37dce5f835771
fep <franck.en.planck@gmail.com>
2019-11-12 15:55:17 +0100

    Bug detection des boutons

--- Appli/src/main/java/fr/input/InputDetection.java
+++ Appli/src/main/java/fr/input/InputDetection.java
@@ -1,35 +1,30 @@
 package fr.input;
 
-import static org.lwjgl.glfw.GLFW.GLFW_JOYSTICK_1;
-import static org.lwjgl.glfw.GLFW.GLFW_JOYSTICK_2;
-import static org.lwjgl.glfw.GLFW.glfwJoystickPresent;
-
-import org.lwjgl.*;
-import org.lwjgl.glfw.*;
-import org.lwjgl.opengl.*;
-import org.lwjgl.system.*;
-
-import java.nio.*;
-
-import static org.lwjgl.glfw.Callbacks.*;
 import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.system.MemoryStack.*;
-import static org.lwjgl.system.MemoryUtil.*;
-
 
 import org.lwjgl.glfw.GLFWErrorCallback;
 
 public class InputDetection {
 	  private boolean joystickPresent1;
 	  private boolean joystickPresent2;
+	  private String nameJoystick1;
+	  private String nameJoystick2;
+	  private int buttonsJ1;
+	  private int count;
+	  
 	  InputDetection(){
 		  init();
-		  joystickPresent1 = glfwJoystickPresent ( GLFW_JOYSTICK_1 );
-		  joystickPresent2 = glfwJoystickPresent ( GLFW_JOYSTICK_2 );
+		  joystickPresent1 = glfwJoystickPresent (GLFW_JOYSTICK_1);
+		  joystickPresent2 = glfwJoystickPresent (GLFW_JOYSTICK_2);
+		  nameJoystick1= glfwGetJoystickName(GLFW_JOYSTICK_1);
+		  nameJoystick2= glfwGetJoystickName(GLFW_JOYSTICK_2);
+		  buttonsJ1=glfwGetJoystickButtons(GLFW_JOYSTICK_1,count);
 	  }
-	  void init(){
+	  
+	void init(){
 
+			glfwInitHint ( GLFW_JOYSTICK_HAT_BUTTONS , GLFW_FALSE );
+			
 			// Setup an error callback. The default implementation
 			// will print the error message in System.err.
 			GLFWErrorCallback.createPrint(System.err).set();
@@ -37,6 +32,13 @@ public class InputDetection {
 			// Initialize GLFW. Most GLFW functions will not work before doing this.
 			if ( !glfwInit() )
 				throw new IllegalStateException("Unable to initialize GLFW"); 
+			
+	  }
+	  String nameDetectJ1(){
+		  return nameJoystick1;
+	  }
+	  String nameDetectJ2(){
+		  return nameJoystick2;
 	  }
 	  boolean joystick1Present() {
 		  return joystickPresent1;
@@ -45,4 +47,8 @@ public class InputDetection {
 	  boolean joystick2Present() {
 		  return joystickPresent2;
 	  }
+
+	public void exitGLFW() {
+		glfwTerminate();
+	}
 }
--- Appli/src/main/java/fr/input/MainInput.java
+++ Appli/src/main/java/fr/input/MainInput.java
@@ -1,19 +1,28 @@
 package fr.input;
-
+/**
+ * https://www.glfw.org/docs/latest/input_guide.html#joystick
+ * @author fep
+ *
+ */
 
 public class MainInput {
 
 	public static void main(String[] args) {
 		boolean inputDetection1;
 		boolean inputDetection2;
+		String typeJ1;
+		String typeJ2;
 		
-		InputDetection IDetection = new InputDetection();
-		inputDetection1=IDetection.joystick1Present();
-		System.out.println("detection joystick 1 :" + inputDetection1);
-		inputDetection2=IDetection.joystick2Present();
-		System.out.println("detection joystick 2 :" + inputDetection2);
+		InputDetection iDetection = new InputDetection();
+		inputDetection1=iDetection.joystick1Present();
+		inputDetection2=iDetection.joystick2Present();
+		typeJ1=iDetection.nameDetectJ1();
+		typeJ2=iDetection.nameDetectJ2();
+		System.out.println("detection joystick 1 :" + inputDetection1+". Il s'agit d'un :"+typeJ1);
+		System.out.println("detection joystick 2 :" + inputDetection2+". Il s'agit d'un :"+typeJ2);
 		
-
+		// Fermeture de la bibliotheque
+		iDetection.exitGLFW();
 	}
 
 }
